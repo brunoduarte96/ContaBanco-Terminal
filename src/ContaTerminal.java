037aa9a0fa1abd690f8c642e48a36609292c3c32
Bruno Duarte <brunoduarte96@outlook.com>
2023-09-20 20:22:42 -0300

    Desafio ContaBanco

---
+++ src/ContaTerminal.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Digite o número da conta: ");
+        int conta = scanner.nextInt();
+
+        System.out.printf("Digite a agência: ");
+        String agencia = scanner.next();
+        scanner.nextLine();
+
+        System.out.printf("Digite o nome cadastrado na conta: ");
+        String nomeCadastrado = scanner.nextLine();
+
+        System.out.printf("Digite o saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCadastrado + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " conta " + conta + " e seu saldo R$" + saldo + " já está disponível para saque");
+
+        scanner.close();
+
+    }
+}
